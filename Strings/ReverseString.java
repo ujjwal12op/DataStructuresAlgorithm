7857548e0e04c44cbfa6b8952945ac36c90c3df9
Ujjwal Tiwari <ujtiwari@gmail.com>
2024-02-28 17:52:56 +0530

    posting dsa jouney

---
+++ Strings/ReverseString.java
@@ -0,0 +1,18 @@
+// Time : O(n)
+// Space: O(1)
+
+
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length-1;
+        while(start<=end)
+        {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
